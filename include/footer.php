698409f89c17a4e9c05fadd7bff9ab1ad37b3a3c
wesleyrayne <111374782+wesleyrayne@users.noreply.github.com>
2023-05-05 17:13:54 +0300

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,55 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/white.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">Get instant access to the widest variety of property listed by trusted landlords and get vacancies in the best apartments effortlessly.</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-white">Frequently Asked Question</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Featured Property</a></li>
+                                                    <li><a href="#" class="text-white">Submit Property</a></li>
+                                                    <li><a href="agent.php" class="text-white">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>Thika,Kenya</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+254712345678</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>0712345678</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>rayne@gmail.com</li>
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
